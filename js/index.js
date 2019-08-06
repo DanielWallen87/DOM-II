7c1649726874f2898bf4ca0707e5c9332e970f0b
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-06 18:09:51 -0400

    Added stopProp plus changed 1 element to modify site style.

--- js/index.js
+++ js/index.js
@@ -35,10 +35,16 @@ nav.forEach(el => el.addEventListener('mouseout', (event) => { // 5: nav reverts
     event.target.style.transition = '0.6s';
 }))
 
-const paras = document.querySelectorAll('p');
-paras.forEach(el => el.addEventListener('mouseup', (event) => { // 6: text highlighted after mouse click ends
-    event.target.style.background = 'lemonchiffon';             // wanted this to be more smooth, but kinda close(?)
-}))
+const text = document.querySelectorAll('.text-content'); // Added this part to implement 
+    text.forEach(el => el.addEventListener('mouseup', (event) => {
+        event.target.style.background = 'pink';
+    }))
+
+const paras = document.querySelectorAll('.text-content p');
+    paras.forEach(el => el.addEventListener('mouseup', (event) => { // 6: text highlighted after mouse click ends
+        event.target.style.background = 'lemonchiffon';             // wanted this to be more smooth, but kinda close(?)
+        event.stopPropagation();
+    }))
 
 nav.forEach(el => el.addEventListener('blur', (event) => { // 7: Nav items change colors upon clicking a link after
     event.target.style.color = 'yellow';                   // the previous one (i.e. can see what's left to explore)
@@ -51,9 +57,14 @@ pics.forEach(el => el.addEventListener('dblclick', (event) => { // 9: Double cli
     event.target.style.display = 'none';                        // Maybe the browser just wants to read the text only??
 }))
 
-body.addEventListener('keydown', function(event) { // 10: Console reveals what keys are being pressed at the moment
-    console.log(`This is my keypress: ${event.code}`);
-  });
+body.addEventListener('keydown', function(event) { 
+    if (event.key === 'f') { // 10: If the browser presses F, they get the below alert
+        alert('F is for FUN bus!');
+    }
+    else {
+        console.log(`This is my keypress: ${event.code}`); // If key other than F, console reveals what it is
+    }
+});
 
 body.addEventListener('keyup', function() { // 11: Console tells us that no keys are being pressed right this second
     console.log('No keys being pressed at the moment');
